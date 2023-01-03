3eb2f043b13d69df1dcf686d3c51662e081d8b3c
Arkadius Schuchhardt <arkadius@schuchhardt.com>
2023-01-03 23:30:45 +0100

    feat: added supervisord and syncthing

--- devices/beowulf/install.sh
+++ devices/beowulf/install.sh
@@ -1,3 +1,20 @@
 # in addition to ansible (TODO: move to ansible)
+
+# supervisor
+sudo apt install supervisor
+- add `chown=arkadius` to /etc/supervisor/supervisord.conf
+[unix_http_server]
+...
+chown=arkadius
+
+
+# access control lists
 sudo apt install acl
 sudo setfacl -R -m u:arkadius:rwx data
+
+# syncthing
+sudo curl -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+sudo apt-get update
+sudo apt-get install syncthing
+
