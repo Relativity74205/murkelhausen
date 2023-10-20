0e05bdbc9d3b1f223604fbdf5024b3cb7ee5005d
Arkadius Schuchhardt <arkadius@schuchhardt.com>
2023-10-20 22:11:57 +0200

    fix: fix to postgres docker compose config

--- devices/beowulf/postgres/superset.sql
+++ devices/beowulf/postgres/superset.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE murkelhausen_app;
 CREATE USER murkelhausen_app WITH PASSWORD '';
-ALTER DATABASE murkelhausen_app OWNER TO murkelhausen_app;
+ALTER DATABASE murkelhausen_app_dev OWNER TO murkelhausen_app;
 
 TRUNCATE TABLE statements_commerzbankauszug;
 SELECT * FROM statements_commerzbankauszug;
